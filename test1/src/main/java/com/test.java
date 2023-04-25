460539c3de23e18e511ed01af4af43c848bcbfe2
pineee <1140420307@qq.com>
2023-04-25 21:55:12 +0800

    push commit 3

--- test1/src/main/java/com/test.java
+++ test1/src/main/java/com/test.java
@@ -15,5 +15,6 @@ public class test {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("push test 2 ssm");
     }
 }
