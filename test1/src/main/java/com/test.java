1c48c4a9f858854378e8c7c8b2acfc602451ebf7
pineee <1140420307@qq.com>
2023-04-25 20:48:10 +0800

    first commit

---
+++ test1/src/main/java/com/test.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @Author: pineee
+ * @Date: created in 2023/4/25 20:25
+ * @Version: 1.0
+ * @Since: 1.0
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
